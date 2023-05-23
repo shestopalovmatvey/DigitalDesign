bc620c2b335aa3f30214aa689a6760a1304ac45c
shestopalovmatvey <shestopalov.myu@edu.spbstu.ru>
2023-05-23 13:58:38 +0300

    modified:   JS/form.js 	modified:   Style/style.css 	modified:   index.html

--- JS/form.js
+++ JS/form.js
@@ -4,11 +4,29 @@ const closeButton = document.querySelector('.purchase-form__close-button');
 const body = document.querySelector('body')
 const form = document.querySelector('.purchase-form__form');
 const valueProduct = document.querySelector('.purchase-form__input');
+const increaseButton = document.querySelector('.quantity-increase');
+const decreaseButton = document.querySelector('.quantity-decrease');
+const MAX_VALUE = parseInt(valueProduct.getAttribute('max'));
+const MIN_VALUE = parseInt(valueProduct.getAttribute('min'));
+
+increaseButton.addEventListener('click', () => {
+  let currentValue = parseInt(valueProduct.value);
+  if (currentValue < MAX_VALUE) {
+      valueProduct.value = currentValue + 1;
+  }
+})
+
+decreaseButton.addEventListener('click', () => {
+  let currentValue = parseInt(valueProduct.value);
+  if (currentValue > MIN_VALUE) {
+      valueProduct.value = currentValue - 1;
+  }
+})
 
 const clearValueInForm = () => {
   purchaseForm.classList.add('hidden');
   body.style.overflow = 'scroll';
-  valueProduct.value = "";
+  valueProduct.value = "1";
 };
 
 buyButtons.forEach((button) => {
